6a6cd11fbca1964877bd444051f460d48b9563f5
KD <kd@KDs-MacBook-Air.local>
2025-04-26 19:31:00 -0700

    created intial home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css';
+export const Home = () => {
+
+    return (
+        <div className="home">
+            <nav className="navbar">
+                <div className="nav-logo">
+                    <img 
+                        src="https://static-assets.bamgrid.com/product/disneyplus/images/logo.1a56f51c764022ee769c91d894d44326.svg"
+                        alt="Disney+"
+                    />
+                </div>
+            </nav>
+           
+        </div>
+    )
+
+}
